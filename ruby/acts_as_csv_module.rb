90f8146b316bd2666a400661b6dd71e3bed2faf5
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2020-12-13 14:21:41 +0100

    Add Ruby code files

---
+++ ruby/acts_as_csv_module.rb
@@ -0,0 +1,59 @@
+module ActsAsCsv
+    def self.included(base)
+        base.extend ClassMethods
+    end
+
+    module ClassMethods
+        def acts_as_csv
+            include InstanceMethods
+        end
+    end
+
+    module InstanceMethods
+        def read
+            @csv_contents = []
+            filename = self.class.to_s.downcase + '.txt'
+            file = File.new(filename)
+            @headers = file.gets.chomp.split(', ')
+        
+            file.each do |row|
+                @csv_contents << row.chomp.split(', ')
+            end
+        end
+
+        def each(&block)
+            @csv_contents.each do |values|
+                row = CsvRow.new(@headers, values)
+                block.call row
+            end
+        end
+
+        attr_accessor :headers, :csv_contents
+        def initialize
+            read
+        end
+    end
+end
+
+class CsvRow
+    def initialize(headers, values)
+        @headers = headers
+        @values = values
+    end
+
+    def method_missing name, *args
+        idx = @headers.index(name.to_s)
+        @values[idx]
+    end
+end
+
+class RubyCsv
+    include ActsAsCsv
+    acts_as_csv
+end
+
+m = RubyCsv.new
+puts m.headers.inspect
+puts m.csv_contents.inspect
+
+m.each {|row| puts row.one}
